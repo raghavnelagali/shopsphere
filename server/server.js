5200b3724397f3753114bbc85d98056d797da954
raghavnelagali <raghunelagali121@gmail.com>
2026-08-13 13:36:03 +0530

    Prepare backend for production deployment

--- server/server.js
+++ server/server.js
@@ -10,6 +10,6 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Start Server
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server running on port ${PORT}`);
 });
